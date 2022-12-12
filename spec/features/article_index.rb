326ee8981a18b349110a710dd2c5e91cb1db95c5
Eric Mielke <ericmielke76@gmail.com>
2022-12-12 15:50:41 -0700

    Create link to create new articles, all tests passing

--- spec/features/article_index.rb
+++ spec/features/article_index.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
-describe 'user sees all articles' do 
-    describe 'they visit /articles' do 
+describe 'Articles Index' do 
+    describe 'a user visits /articles' do 
         let! (:article_1) {Article.create!(title: "Title 1", body: "Body 1")}
         let! (:article_2) {Article.create!(title: "Title 2", body: "Body 2")}
 
         before :each do 
             visit "/articles"
         end 
+
         it 'displays all articles' do 
-            expect(page).to have_content(article_1.title)
-            expect(page).to have_content(article_2.title)
+            expect(page).to have_link(article_1.title)
+            expect(page).to have_link(article_2.title)
+        end
+
+        it 'has a link to create a new article' do 
+            expect(page).to have_link("Create a New Article")
         end
     end
 end
